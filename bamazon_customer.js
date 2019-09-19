39610a44144a6ec249887f6abf21dd648c47e4d4
Jesse Denick <jessedenick@gmail.com>
2019-09-19 16:51:15 -0400

    added customerChoice function, function does work as intended

--- bamazon_customer.js
+++ bamazon_customer.js
@@ -12,4 +12,65 @@ var connection = mysql.createConnection({
 connection.connect(function(err){
     if (err) throw err;
     console.log("connection successful");
+    displayTable();
 })
+
+var displayTable = function(){
+    connection.query("SELECT * FROM products", function(err, res){
+        for(var i=0; i<res.length; i++){
+            console.log(
+                "Item Number: " + res[i].itemId + "\n" +
+                "Item: " + res[i].productName + "\n" +
+                "Department Name: " + res[i].departmentName + "\n" + 
+                "Price: " + res[i].price + "\n" +
+                "Quantity: " + res[i].stockQuantity + "\n" +
+                "*********************************");
+        }
+       customerChoice(res);
+    })
+}
+
+var customerChoice = function(res){
+    inquirer.prompt([{
+        type: "input",
+        name: "choice",
+        message: "What would you like to purchase?"
+    }]).then(function(answer){
+        var correct = false;
+        for(var i=0; i<res.length; i++){
+            if(res[i].productName==answer.choice){
+                correct=true;
+                var product=answer.choice;
+                var id=i;
+                inquirer.prompt({
+                    type: "input",
+                    name: "Quantity",
+                    message: "How many would you like to buy?",
+                    validate: function(value){
+                            if(isNaN(value)==false){
+                                return true;
+                            } else {
+                                return false;
+                            } 
+                        }
+                    }).then(function(answer){
+                        if((res[id].stockQuantity-answer.Quantity)>0){
+                            connection.query("UPDATE products SET stockQuantity='"+(res[id].stockQuantity-
+                            answer.Quantity)+"' WHERE productName='"+product
+                            +"'", function(err, res2){
+                            console.log("DATA HAS BEEN UPDATED:")
+                            displayTable();
+                            console.log("Congratulations, product has been purchased!");
+                        })
+                    } else {
+                        console.log("Not a valid Selection, please select another choice!");
+                        displayChoice(res);
+                    }
+                })
+            }
+        }
+    })
+}
+        
+
+        
